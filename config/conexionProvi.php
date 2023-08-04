50421035c052a0601c618934629fb4030ff3ae62
dpdesarrollo1 <dpdesarrollocanaima@gmail.com>
2023-08-04 14:22:22 -0400

    Correccion de conexion

--- config/conexionProvi.php
+++ config/conexionProvi.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$mysqli = new mysqli("localhost", "root", "", "oac_prueba");
+$mysqli = new mysqli("10.10.5.28", "sistema", "123456", "oac_prueba");
 
 if ($mysqli->connect_error) {
     die("Conexion Fallo:" . $mysqli->connect_error);
