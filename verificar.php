b8efa857f8a6bbbefa35dd6c42d11167b55f85c6
DpSistemaCanaima <dpdesarrollocanaima@gmail.com>
2023-08-15 10:05:04 -0400

    Comprobaciones de verificador

--- verificar.php
+++ verificar.php
@@ -10,6 +10,7 @@ if (!isset($_SESSION['id_usuarios']) OR !isset($_GET['Observacion'])) {
 }
 
 $observacionT = $mysqli->real_escape_string($_GET['Observacion']);
+$comprobacion = implode(',', $_POST['comprobaciones']);
 $estatus = $mysqli->real_escape_string($_GET['id_status']);
 $responsable = $mysqli->real_escape_string($_GET['responsable']);
 $id_roles = $mysqli->real_escape_string($_GET['id_roles']);
@@ -18,7 +19,7 @@ $idDispo = $mysqli->real_escape_string($_GET['id_dispositivo']);
 
 //$sql = "UPDATE datos_del_dispotivo SET id_estatus = ".$estatus.",  observaciones = '".$observacionT."', responsable = '".$responsable."', id_roles = '".$id_roles."'  WHERE id_datos_del_dispositivo = ".$_SESSION['lastId']; 
 
-$sql = "UPDATE datos_del_dispotivo SET id_estatus = ".$estatus.",  observaciones = '".$observacionT."', responsable = '".$responsable."', id_roles = '".$id_roles."'  WHERE id_datos_del_dispositivo = ".$idDispo; 
+$sql = "UPDATE datos_del_dispotivo SET id_estatus = ".$estatus.",  observaciones = '".$observacionT."', comprobaciones = '".$comprobacion."' responsable = '".$responsable."', id_roles = '".$id_roles."'  WHERE id_datos_del_dispositivo = ".$idDispo; 
 
 $resultado = $mysqli->query($sql);
 
