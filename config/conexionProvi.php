40a08c1a5e87732f3647ca6b326a5d544b9cf496
QuizRoa <aquizhpi4@gmail.com>
2023-09-19 14:50:41 -0400

    notificaciones analista

--- config/conexionProvi.php
+++ config/conexionProvi.php
@@ -1,12 +1,7 @@
 <?php
-<<<<<<< HEAD
 $mysqli = new mysqli("localhost", "root", "", "oac_prueba");
-=======
->>>>>>> 1a9b38516d8e9f8431c0106909ec22fb8c8615b8
 
-//$mysqli = new mysqli("localhost", "root", "", "oac_prueba");
-
-$mysqli = new mysqli("10.10.5.28", "sistema", "123456", "oac_prueba");
+//$mysqli = new mysqli("10.10.5.28", "sistema", "123456", "oac_prueba");
 
 if ($mysqli->connect_error) {
     die("Conexion Fallo:" . $mysqli->connect_error);
