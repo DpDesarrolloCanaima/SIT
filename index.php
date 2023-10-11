9de89342fcf4a5b783662ece1b1c0e2acf001f58
danyerbert <danyerbert@gmail.com>
2023-10-11 02:44:50 -0400

    Documentación de cambios y correcion de conexión

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ if ($_POST) {
             $_SESSION['id_usuarios'] = $row['id_usuarios'];
             $_SESSION['usuario'] = $row['usuario'];
             $_SESSION['id_roles'] = $row['id_roles'];
+            //Creación de la sessión para manipular la información a mostrar en el registro historico.
             $_SESSION['full_identificacion'] = 'C.I: '.$row['cedula'].', USUARIO: '.$row['usuario'].', NOMBRE: '.$row['nombre'];
         //    Comprobación de inicion de sesión y roles
             if (isset($_SESSION['id_roles'])) {
