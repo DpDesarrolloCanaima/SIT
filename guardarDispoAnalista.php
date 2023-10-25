86c7f888065079e66974a536e75eaab91dfe16d1
danyerbert <danyerbert@gmail.com>
2023-10-25 14:50:54 -0400

    Correcion de redireccinamiento de registro dispositivo

--- guardarDispoAnalista.php
+++ guardarDispoAnalista.php
@@ -48,7 +48,7 @@ require("function.php");
                                 confirmButtonText: 'OK',
                                 timer: 1500
                               }).then(() => {
-                                location.assign('listadebeneficiario.php');
+                                location.assign('dispositivoentrada.php');
                               });
                     });
                         </script>";
@@ -66,7 +66,7 @@ require("function.php");
                                 confirmButtonText: 'OK',
                                 timer: 1500
                               }).then(() => {
-                                location.assign('listadebeneficiario.php');
+                                location.assign('dispositivoentrada.php');
                              });
                     });
                         </script>";
@@ -88,7 +88,7 @@ require("function.php");
                         confirmButtonText: 'OK',
                         timer: 1500
                       }).then(() => {
-                        location.assign('listadebeneficiario.php');
+                        location.assign('analista.php');
                      });
             });
                 </script>";  
@@ -106,7 +106,7 @@ require("function.php");
                                 confirmButtonText: 'OK',
                                 timer: 1500
                               }).then(() => {
-                                location.assign('listadebeneficiario.php');
+                                location.assign('analista.php');
                              });
                     });
                         </script>";
