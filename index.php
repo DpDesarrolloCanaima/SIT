a3d105bbbc295fae2d09c0618977dff011d6f677
danyerbert <danyerbert@gmail.com>
2024-04-22 11:52:17 -0400

    Estructura de carpetas y archivos

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet"/>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="css/estilos.css">
-  <title>Login</title>
+  <title>Sistema de Inventario y Trzabilidad | Produccion</title>
 </head>
 <body class="vh-100" style="border: 1px solid black;">
   <section class="h-100 gradient-form" style="background-color: #eee;">
