d106bf881cd577454fa4f15862910da8ce169118
danyerbert <danyerbert@gmail.com>
2024-06-04 09:27:16 -0400

    Correciones de deslogeo y validacion de sesion

--- content/inc/navbar.php
+++ content/inc/navbar.php
@@ -3,13 +3,7 @@
             <a class="navbar-brand ps-3" href="home.html">SIT | Produccion</a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-            <!-- Navbar Search-->
-            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-                <div class="input-group">
-                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                    <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
-                </div>
-            </form>
+            
             <!-- Navbar-->
             <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                 <li class="nav-item dropdown">
@@ -17,7 +11,7 @@
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" href="#!">Configuracion</a></li>
                         <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="#!">Salir</a></li>
+                        <li><a class="dropdown-item" href="logout.php">Salir</a></li>
                     </ul>
                 </li>
             </ul>
--- listadepersonas.php
+++ listadepersonas.php
@@ -1,4 +1,12 @@
 <?php 
+session_start();
+if (!isset($_SESSION['id_usuario'])) {
+    header("Location: index.php");
+}else{
+    if ($_SESSION['rol'] != 1) {
+        header("Location: 404.php");
+    }
+}
 require "config/conexion.php";
 
 include "content/inc/header.php";
